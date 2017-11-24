ad04bcd948367c3cf097a1c7cb3ac81401239a6c
Andrzej Ekiert <andrzej.ekiertt@gmail.com>
2017-11-24 20:02:36 +0100

    gif

---
+++ src/main/java/com/example/gifsproject/GifsprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.gifsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifsprojectApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifsprojectApplication.class, args);
+	}
+}
